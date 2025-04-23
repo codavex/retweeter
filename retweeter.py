57487a07b53aa0885c24edfcd1f139e2af49c7cf
David Cox <dave@codavex.co.uk>
2025-04-23 15:59:55 +0100

    progress

--- retweeter.py
+++ retweeter.py
@@ -9,10 +9,8 @@ import logging.handlers
 import tweepy
 
 
-def setup_api(consumer_key, consumer_secret, access_key, access_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    return tweepy.API(auth)
+def setup_api(bearer_token):
+    return tweepy.Client(bearer_token)
 
 
 def blacklist_match(text, blacklist):
@@ -23,10 +21,10 @@ def blacklist_match(text, blacklist):
 
 
 def already_retweeted(tweet_to_check):
-    if hasattr(tweet_to_check, 'retweeted_status'):
-        for retweet in api.get_retweets(tweet_to_check.retweeted_status.id):
-            if whoami.screen_name in retweet.user.screen_name:
-                return True
+    #if hasattr(tweet_to_check, 'retweeted_status'):
+         #for retweet in api.get_retweets(tweet_to_check.retweeted_status.id):
+         #   if USERNAME in retweet.user.screen_name:
+         #       return True
     return False
 
 
@@ -38,11 +36,9 @@ Config = configparser.ConfigParser()
 Config.read(sys.argv[1])
 
 # get the authentication information from the ini file
-CONSUMER_KEY = Config.get("Authentication", "CONSUMER_KEY")
-CONSUMER_SECRET = Config.get("Authentication", "CONSUMER_SECRET")
-ACCESS_KEY = Config.get("Authentication", "ACCESS_KEY")
-ACCESS_SECRET = Config.get("Authentication", "ACCESS_SECRET")
+BEARER_TOKEN = Config.get("Authentication", "BEARER_TOKEN")
 
+USERNAME = Config.get("Settings", "USERNAME")
 THROTTLE = Config.getint("Settings", "THROTTLE")
 EXCLUDE_MENTIONS = Config.getboolean("Settings", "EXCLUDE_MENTIONS")
 RESTRICT_USERS = Config.getint("Settings", "RESTRICT_USERS")
@@ -57,24 +53,56 @@ logging.config.fileConfig(LOG_CONFIG)
 logger = logging.getLogger(__name__)
 
 # authenticate and setup tweepy
-api = setup_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+client = setup_api(BEARER_TOKEN)
 
 # get my user details
-whoami = api.get_user()
-logger.info("whoami: %s", whoami.screen_name)
+logger.info("USERNAME: %s", USERNAME)
+
+response = []
+try:
+    response = client.get_user(username=USERNAME)
+except tweepy.errors.TweepyException as e:
+    logger.error('Error looking up %s, %s', USERNAME, e)
+    sys.exit(-1)
+
+my_id = response.data.id
+
+# get more recent tweets by USERNAME
+try:
+    response = client.get_users_tweets(
+        id=my_id,
+        # tweet_fields=['author_id', 'entities'],
+        # user_fields=['username', 'public_metrics', 'description', 'location'],
+        # tweet_fields=['author_id','created_at', 'geo', 'public_metrics', 'text'],
+        expansions=['author_id', 'entities.mentions.username'],
+        user_fields=['username'],
+        tweet_fields=['author_id', 'created_at', 'referenced_tweets', 'text'],
+        #expansions=['author_id'],
+
+        max_results=RESTRICT_USERS,
+        )
+except tweepy.errors.TweepyException as e:
+    logger.error('Error getting recent retweets %s', e)
+    sys.exit(-1)
 
 # get id of most recent tweet
-most_recent_tweet = api.user_timeline(count=1)[0]
+recent_tweets = response.data
+recent_authors = response.includes
+most_recent_tweet = response.data[0]
 logger.debug("Most recent tweet: %s", most_recent_tweet.text)
+
+print(recent_authors)
+
+for tweet in recent_tweets:
+    logger.debug("%s %s", tweet.author_id, tweet.text)
 
 # get last X tweets authors
 retweeted_authors = []
-
 if RESTRICT_USERS:
-    recent_tweets = api.user_timeline(count=RESTRICT_USERS)
-
-    for tweet in recent_tweets:
-        retweeted_authors.append(tweet.entities['user_mentions'][0]['screen_name'])
+    for user in recent_authors['users']:
+        retweeted_authors.append(
+            user.username
+        )
 
 logger.debug("retweeted_authors: %s", retweeted_authors)
 
@@ -82,21 +110,43 @@ logger.debug("retweeted_authors: %s", retweeted_authors)
 results = []
 
 for search_string in SEARCH_TERMS.split(","):
-    results += reversed(api.search_tweets(q="\""+search_string+"\"", since_id=most_recent_tweet.id, result_type='recent'))
+    print(search_string)
+
+results += reversed(
+    client.search_recent_tweets(
+        query="\"is my next band name\"",
+        # tweet_fields=['author_id', 'entities', 'referenced_tweets'],
+        # user_fields=['username'],
+        user_fields=['username', 'public_metrics', 'description', 'location'],
+        tweet_fields=['author_id','created_at', 'geo', 'public_metrics', 'text'],
+        # expansions=['author_id', 'entities.mentions.username'],
+        expansions=['author_id'],
+        max_results=100,
+    )
+)
 
 # if we've had multiple search terms, sort to get back in time order
-results.sort(key=lambda result: result.id)
+# results.sort(key=lambda result: result.id)
 
-logger.info("Found %d tweets", len(results))
+print(results[2])
+print(results[3])
+print("\n\n\n")
+logger.info("Found %d tweet(s)", results[0]['result_count'])
+
+print(results)
 
 # retweet what we've found.
-for result in results:
-    logger.info("Tweet under consideration: %s - %s", result.author.screen_name, result.text)
+for result in results[3]:
+    logger.info(
+            "Tweet under consideration: %d - %s",
+            result.id,
+            result.text
+            )
 
     if TRIAL_RUN:
         # do nothing
         logger.info("TRIAL RUN - not retweeting")
-    elif EXCLUDE_MENTIONS and whoami.screen_name in result.text:
+    elif EXCLUDE_MENTIONS and USERNAME in result.text:
         # do nothing if we're excluding mentions and mentioned
         logger.info("Mentioned in tweet - not retweeting")
     elif RESTRICT_USERS and result.author.screen_name in retweeted_authors:
@@ -111,7 +161,7 @@ for result in results:
     else:
         logger.info("Retweeting")
         try:
-            api.retweet(result.id)
+            client.retweet(result.id)
             retweeted_authors.append(result.author.screen_name)
             time.sleep(THROTTLE)
         except tweepy.errors.TweepyException as e:
