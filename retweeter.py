479436d847d9e89660dc1a40e34d4cbd5cfa489e
David Cox <dave@codavex.co.uk>
2025-04-25 12:44:31 +0100

    further progress

--- retweeter.py
+++ retweeter.py
@@ -10,7 +10,7 @@ import tweepy
 
 
 def setup_api(bearer_token):
-    return tweepy.Client(bearer_token)
+    return tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
 
 
 def blacklist_match(text, blacklist):
@@ -20,11 +20,17 @@ def blacklist_match(text, blacklist):
     return False
 
 
-def already_retweeted(tweet_to_check):
-    #if hasattr(tweet_to_check, 'retweeted_status'):
-         #for retweet in api.get_retweets(tweet_to_check.retweeted_status.id):
-         #   if USERNAME in retweet.user.screen_name:
-         #       return True
+def already_retweeted(tweet_id, my_id):
+    try:
+        retweets_response = client.get_retweeters(id = tweet_id)
+    except tweepy.errors.TweepyException as e:
+        logger.error('Error looking up %s, %s', USERNAME, e)
+        sys.exit(-1)
+    users = retweets_response.data
+    if users:
+        for user in users:
+            if user.id == my_id:
+                return True
     return False
 
 
@@ -35,7 +41,7 @@ if len(sys.argv) != 2:
 Config = configparser.ConfigParser()
 Config.read(sys.argv[1])
 
-SEARCH_TERMS = Config.get("Search", "SEARCH_TERMS")
+SEARCH_TERM = Config.get("Search", "SEARCH_TERM")
 BLACKLIST_TERMS = Config.get("Search", "BLACKLIST_TERMS")
 
 BASE_CONFIG = Config.get("Settings", "BASE_CONFIG")
@@ -74,14 +80,9 @@ my_id = response.data.id
 try:
     response = client.get_users_tweets(
         id=my_id,
-        # tweet_fields=['author_id', 'entities'],
-        # user_fields=['username', 'public_metrics', 'description', 'location'],
-        # tweet_fields=['author_id','created_at', 'geo', 'public_metrics', 'text'],
         expansions=['author_id', 'entities.mentions.username'],
         user_fields=['username'],
         tweet_fields=['author_id', 'created_at', 'referenced_tweets', 'text'],
-        #expansions=['author_id'],
-
         max_results=RESTRICT_USERS,
         )
 except tweepy.errors.TweepyException as e:
@@ -94,75 +95,62 @@ recent_authors = response.includes
 most_recent_tweet = response.data[0]
 logger.debug("Most recent tweet: %s", most_recent_tweet.text)
 
-print(recent_authors)
-
-for tweet in recent_tweets:
-    logger.debug("%s %s", tweet.author_id, tweet.text)
-
 # get last X tweets authors
 retweeted_authors = []
 if RESTRICT_USERS:
-    for user in recent_authors['users']:
+    for author in recent_authors['users']:
         retweeted_authors.append(
-            user.username
+            author.username
         )
 
 logger.debug("retweeted_authors: %s", retweeted_authors)
 
 # get tweets
 results = []
-
-results += reversed(
-    client.search_recent_tweets(
-        query=SEARCH_TERM,
-        # tweet_fields=['author_id', 'entities', 'referenced_tweets'],
-        # user_fields=['username'],
-        user_fields=['username', 'public_metrics', 'description', 'location'],
-        tweet_fields=['author_id','created_at', 'geo', 'public_metrics', 'text'],
-        # expansions=['author_id', 'entities.mentions.username'],
+try:
+    results = client.search_recent_tweets(
+        query=f"\"{SEARCH_TERM}\"",
+        user_fields=['username'],
+        tweet_fields=['author_id','created_at', 'referenced_tweets', 'text'],
         expansions=['author_id'],
         max_results=100,
     )
-)
+except tweepy.errors.TweepyException as e:
+    logger.error('Error getting searching for tweets: %s', e)
+    sys.exit(-1)
 
-# if we've had multiple search terms, sort to get back in time order
-# results.sort(key=lambda result: result.id)
+logger.info("Found %d tweet(s)", results[3]['result_count'])
 
-print(results[2])
-print(results[3])
-print("\n\n\n")
-logger.info("Found %d tweet(s)", results[0]['result_count'])
-
-print(results)
-
+found_tweets = reversed(results[0])
 # retweet what we've found.
-for result in results[3]:
+for tweet in found_tweets:
     logger.info(
-            "Tweet under consideration: %d - %s",
-            result.id,
-            result.text
+            "Tweet under consideration: %d - %s - %s",
+            tweet.id,
+            tweet.text,
+            tweet.created_at
             )
-
     if TRIAL_RUN:
         # do nothing
         logger.info("TRIAL RUN - not retweeting")
-    elif EXCLUDE_MENTIONS and USERNAME in result.text:
+    elif EXCLUDE_MENTIONS and USERNAME in tweet.text:
         # do nothing if we're excluding mentions and mentioned
         logger.info("Mentioned in tweet - not retweeting")
-    elif RESTRICT_USERS and result.author.screen_name in retweeted_authors:
+    elif RESTRICT_USERS and tweet.author.screen_name in retweeted_authors:
         # if users has been retweeted recently, ignore
         logger.info("User retweeted recently - not retweeting")
-    elif blacklist_match(result.text, BLACKLIST_TERMS):
+    elif blacklist_match(tweet.text, BLACKLIST_TERMS):
         # tweet matches blacklisted terms, ignore
         logger.info("Blacklist hit - not retweeting")
-    elif already_retweeted(result):
+    elif tweet.referenced_tweets and \
+        already_retweeted(tweet.referenced_tweets[0].id, my_id):
         # if I've already retweeted, ignore
         logger.info("Already retweeted - not retweeting")
     else:
         logger.info("Retweeting")
         try:
-            client.retweet(result.id)
-            retweeted_authors.append(result.author.screen_name)
+            client.retweet(tweet.id)
+            retweeted_authors.append(tweet.author.screen_name)
             time.sleep(THROTTLE)
         except tweepy.errors.TweepyException as e:
             logger.error('Error retweeting: %s', e)
